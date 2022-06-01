9ba06e771594ef9c97716a5a06d6d5753026e852
stachu1190 <stachu1190@gmail.com>
2022-06-01 16:44:40 +0200

    implemented greedy algorithm

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 fun main(args: Array<String>) {
-    val sequence = Sequence();
-    sequence.initSequence("instance_test.txt");
-    println(sequence.toString());
-    println(sequence.getOffsetSum())
-    println(sequence.getString())
-    println(sequence.getStringLength())
-    println(sequence.toString());
+    val sequence = Sequence("pos_random_209+80-2.txt");
+    println(sequence.getStringLength());
+    val sequence2 = sequence.initSequence(209);
+    println(sequence2.getStringLength());
+    println(sequence2.toString())
+    println(sequence2.getSize());
+    println("length = ${sequence2.getStringLength()}")
+    println("quality = ${sequence2.getSize().toFloat()/(sequence.getSize().toFloat()-80)}")
 }
--- src/main/kotlin/Sequence.kt
+++ src/main/kotlin/Sequence.kt
@@ -1,11 +1,70 @@
 class Sequence {
-    lateinit var set: MutableList<Oligonucleotide>;
+    var set: MutableList<Oligonucleotide>;
 
-    fun initSequence(fileName: String) {
+    constructor(list: MutableList<Oligonucleotide>) {
+        this.set = list;
+    }
+
+    constructor(fileName: String) {
         val dataReader = DataReader();
         dataReader.readInstance(fileName)
-        this.set = dataReader.getSet()
+        var instance = dataReader.getSet();
+        this.set = instance;
         calcAllOffset();
+    }
+
+    fun addElement(element: Oligonucleotide) {
+        this.set += element;
+    }
+
+    fun isNotEmpty(): Boolean {
+        return this.set.isNotEmpty();
+    }
+
+    fun removeElementAt(index: Int) {
+        this.set.removeAt(index);
+    }
+
+    fun initSequence(wantedLength: Int): Sequence {
+        var elemCount = 0;
+        var bestSeq = this;
+        for (i in this.set.indices) {
+            var currentSeq = greedyAlghoritm(this.set, i, wantedLength);
+            if(elemCount < currentSeq.getSize()) {
+                bestSeq = currentSeq;
+            }
+            println(currentSeq.getSize())
+        }
+        return bestSeq;
+    }
+
+    private fun greedyAlghoritm(nucleotidesSet: MutableList<Oligonucleotide>, startIndex: Int, wantedLength: Int): Sequence {
+        var seq = Sequence(mutableListOf<Oligonucleotide>());
+        val instance = nucleotidesSet.toMutableList()
+        var current = instance[startIndex];
+        seq.addElement(current);
+        instance.removeAt(startIndex);
+        while (instance.isNotEmpty()) {
+            var min = 1000; // we won't handle oligonucleotides as long as 1000 char
+            var minIndex = -1;
+            for (i in instance.indices) {
+                if(calcOffset(current.getValue(), instance[i].getValue()) < min) {
+                    min = calcOffset(current.getValue(), instance[i].getValue());
+                    minIndex = i;
+                }
+            }
+            if(seq.getStringLength() + calcOffset(current.getValue(), instance[minIndex].getValue()) > wantedLength) {
+                break;
+            }
+            else
+            {
+                seq.addElement(instance[minIndex]);
+                current = instance[minIndex];
+                instance.remove(instance[minIndex]);
+            }
+        }
+        seq.calcAllOffset();
+        return seq;
     }
 
     private fun calcOffset(string1: String, string2: String): Int {
@@ -28,7 +87,7 @@ class Sequence {
             } else {
                 set[i].setOffset(0);
                 val offset = calcOffset(set[i - 1].getValue(), set[i].getValue());
-                set[i - 1].setOffset(set[i - 1].getOffset() + offset);
+                //set[i - 1].setOffset(set[i - 1].getOffset() + offset);
                 set[i].setOffset(set[i].getOffset() + offset);
             }
         }
@@ -44,6 +103,10 @@ class Sequence {
     }
 
 //  TODO: consider calculating length based on offsets, not on sequence string
+
+//    fun getSequenceLength(): Int {
+//
+//    }
     fun getStringLength(): Int {
         val string = getString()
         return string.length
@@ -65,7 +128,8 @@ class Sequence {
         val temp = set[index1];
         set[index1] = set[index2];
         set[index2] = temp;
-        calcAllOffset()
+        calcAllOffset();
+
     }
 
     fun getOffsetSum(): Int {
